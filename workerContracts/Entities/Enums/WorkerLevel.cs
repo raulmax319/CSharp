1bca52d6ffcd827ee1e82968ba448760437066b3
raulmax <raulmax319@gmail.com>
2021-01-06 15:06:31 -0300

    Fixed Enums names to correctly correspond to the given string

--- workerContracts/Entities/Enums/WorkerLevel.cs
+++ workerContracts/Entities/Enums/WorkerLevel.cs
@@ -1,7 +1,7 @@
 namespace workerContracts.Entities.Enums {
     enum WorkerLevel : int {
-        JUNIOR,
-        MID_LEVEL,
-        SENIOR
+        Junior,
+        MidLevel,
+        Senior
     }
 }
