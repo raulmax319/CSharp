6c85b07595c5ea117017f880100065c490512834
raulmax <raulmax319@gmail.com>
2021-01-07 20:34:50 -0300

    Added Class Position

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Board;
 
 namespace xadrez_console
 {
---
+++ xadrez-console/board/Position.cs
@@ -0,0 +1,16 @@
+namespace Board {
+    class Position {
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+        public Position(int line, int column) {
+            this.Line = line;
+            this.Column = column;
+        }
+
+        public override string ToString()
+        {
+            return $"{Line}, {Column}";
+        }
+    }
+}
